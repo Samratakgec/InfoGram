582b4c62a79697302566c6bca6c3737f61e28225
Samratakgec <113331601+Samratakgec@users.noreply.github.com>
2025-03-31 11:19:03 +0530

    Delete src/main/java/org/example/utility directory

--- src/main/java/org/example/utility/ConnectionProvider.java
+++
@@ -1,9 +0,0 @@
-package org.example.utility;
-import java.sql.* ;
-public class ConnectionProvider {
-    static public Connection getConnection() throws SQLException
-    {
-        String url="jdbc:mysql://localhost:3306/anudip";
-        return DriverManager.getConnection(url,"root","tiger");
-    }
-}
